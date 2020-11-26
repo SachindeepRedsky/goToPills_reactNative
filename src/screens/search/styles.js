1895c6c4356ba7f9be6e0da336b312b789a99b60
Sumit Sangwal <sumit0111.dcsa@gmail.com>
2020-11-27 02:14:25 +0530

    api integration in search screen

--- src/screens/search/styles.js
+++ src/screens/search/styles.js
@@ -1,152 +1,204 @@
-import {StyleSheet} from "react-native";
-import {spacings} from '../../constants/fonts';
+import { StyleSheet } from "react-native";
+import { spacings } from '../../constants/fonts';
 import * as colors from '../../constants/colors';
 
 
 const styles = StyleSheet.create({
-   container:{
-                flex:1,
-                // alignItems:'center'
-   },
-   textstyle:{
-    fontSize:26,
-    fontWeight:'bold',
-    color:'#8ea6e6',
-    textAlign:'center',
-    marginTop:spacings.xxsmall
-   },
-   menuicon:{
-     width:15,
-     height:15,
-     marginLeft:10
-   },
-   inputBox:{
-      //borderBottomWidth:2,
-      width:320,
-      height:40,
-      marginLeft:20,
-     
-   },
-   horizontalline:{
-     marginLeft:13,
-     width:310,
-     height:1,
-     backgroundColor:'#dadada',
-     shadowColor: "#000",
-     shadowOffset: {
-       width: 0,
-       height: 1,
-     },
-     shadowOpacity: 0.18,
-     shadowRadius: 1.00,
-     
-     elevation: 1,
-        },
-        dropDownLine:{
-          width:120,
-          height:1,
-          marginRight:20,
-     backgroundColor:'#adadad',
-     shadowColor: "#000",
-     shadowOffset: {
-       width: 0,
-       height: 1,
-     },
-     shadowOpacity: 0.18,
-     shadowRadius: 1.00,
-     
-     elevation: 1,
-        },
-        dropDownLine1:{
-          width:140,
-          height:1,
-     backgroundColor:'#adadad',
-     shadowColor: "#000",
-     shadowOffset: {
-       width: 0,
-       height: 1,
-     },
-     shadowOpacity: 0.18,
-     shadowRadius: 1.00,
-     
-     elevation: 1,
-        },
-   Text:{
-          fontSize:16,
-          color:colors.headerColor,
-          paddingTop:15,
-          paddingLeft:15
-   },
-   Textinputbox:{
-     color:'#a0a0a0',
-     paddingTop:16
-   },
-   inputView:{
-     //flex:1,
-     //justifyContent:'center',
-     //marginTop:20,
-     alignItems:'stretch'
-   },
-   middleView:{
-                flexDirection:'row',
-                //alignItems:'stretch',
-                marginTop:20,
-                justifyContent:'space-between'
-   },
-   MiddleFirstView:{
-          marginLeft:10,
-   },
-   MiddleSecondView:{
-     alignItems:'flex-end'
-   },
-   inputBox1:{
-        borderBottomWidth:2,
-        borderBottomColor:'#dadada',
-        width:140,
-        height:30
-   },
-   dropDown:{
-     height:50,
-     width:150,
-     borderWidth:1
-   },
-   Button:{
-     marginTop:25,
-      backgroundColor:colors.btnColor,
-      width:320,
-      height:45,
-      borderRadius:8
-   },
-   ButtonText:{
-     //justifyContent:'center',
-     //alignItems:'center',
-     textAlign:'center',
-     fontSize:25,
-     fontWeight:'bold',
-     color:'#1b5987',
-     paddingTop:5
-   },
-   BottomView:{
-       alignItems:'center',
-       justifyContent:'center',
-       margin:4,
-   },
-   BottomView1:{
-     borderLeftWidth:1,
-     borderLeftColor:'#5faafd',
-     marginLeft:2,
-     alignItems:'center',
-     justifyContent:'center'
-   },
-   buttonView:{
-     justifyContent:'center',
-     alignItems:'center'
-   },
-   BottomText:{
-     color: colors.headerColor,
-     fontSize:12,
-     textDecorationLine:'underline',
+  container: {
+    flex: 1,
+    // alignItems:'center'
+    width: '100%',
+    height: '100%',
+  },
+  textstyle: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#8ea6e6',
+    textAlign: 'center',
+    marginTop: spacings.xxsmall,
+    marginBottom: spacings.medium
+  },
+  menuicon: {
+    width: 15,
+    height: 15,
+    marginLeft: 10
+  },
+  inputBox: {
+    //borderBottomWidth:2,
+    width: '100%',
+    height: 40,
+    // borderBottomWidth: 2,
+    // borderBottomColor: '#dadada',
+    padding: 0
+  },
+  horizontalline: {
+    // marginLeft: 13,
+    width: '100%',
+    height: 1,
+    backgroundColor: '#dadada',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1.00,
+
+    elevation: 1,
+  },
+  dropDownLine: {
+    width: '80%',
+    height: 1,
+    backgroundColor: '#adadad',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1.00,
+    elevation: 1,
+    alignSelf: 'flex-end'
+  },
+  dropDownLine1: {
+    width: '80%',
+    height: 1,
+    backgroundColor: '#adadad',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1.00,
+    elevation: 1,
+  },
+  Text: {
+    fontSize: 16,
+    color: '#006596',
+    marginBottom: 5,
+    width: '100%',
+  },
+  Textinputbox: {
+    color: '#a0a0a0',
+    paddingTop: 16
+  },
+  inputView: {
+    //flex:1,
+    //justifyContent:'center',
+    //marginTop:20,
+    alignItems: 'stretch',
+    width: '100%',
+    height: '100%'
+  },
+  middleView: {
+    flexDirection: 'row',
+    //alignItems:'stretch',
+    width: '90%',
+    justifyContent: 'space-between'
+  },
+  MiddleFirstView: {
+    // marginLeft: 10,
+    width: '50%',
+    // height: 400
+  },
+  MiddleSecondView: {
+    alignContent: 'flex-end',
+    width: '50%'
+  },
+  inputBox1: {
+    borderBottomWidth: 2,
+    borderBottomColor: '#dadada',
+    width: '80%',
+    height: 30,
+    padding: 0
+  },
+  focusedInputBox1: {
+    borderBottomWidth: 2,
+    borderBottomColor: colors.headerColor,
+    width: '80%',
+    height: 30,
+    padding: 0
+  },
+  dropDown: {
+    height: 50,
+    width: '80%',
+    borderWidth: 1,
+    alignSelf: 'flex-end',
+    marginTop: 15
+  },
+  dropDown1: {
+    height: 50,
+    width: '80%',
+    borderBottomWidth: 2,
+    borderBottomColor: '#dadada'
+  },
+  Button: {
+    marginTop: 25,
+    backgroundColor: colors.btnColor,
+    width: '100%',
+    height: 45,
+    borderRadius: 8
+  },
+  ButtonText: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#1b5987',
+    paddingTop: 5
+  },
+  BottomView: {
+    alignItems: 'center',
+    height: 200,
+    marginTop: 5
+  },
+  buttonView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 300
+  },
+  BottomText: {
+    color: colors.headerColor,
+    fontSize: 12,
+    textDecorationLine: 'underline',
     //  letterSpacing:1
-   }
+  },
+  // autoCompleteContainer: {
+  //   height: 10,
+  //   backgroundColor:'red'
+  // },
+
+  autoCompleteContainer: {
+    height: 0,
+  },
+  autoCompleteContainerAuto: {
+    height: 'auto',
+  },
+  autoCompleteContainer1: {
+    height: 0,
+  },
+  autoCompleteContainer1Auto: {
+    height: 'auto',
+  },
+  autocompleteText: {
+    width: '100%',
+    height: 40,
+    fontSize: 16
+  },
+  autocompleteListContainerStyle: {
+    width: '100%',
+    borderWidth: 0,
+    // backgroundColor: 'red'
+  },
+  autocompleteListStyle: {
+    width: '100%',
+    marginLeft: 0,
+    backgroundColor: '#fafafa'
+  },
+  autocompleteInputContainer: {
+    width: '100%',
+    paddingLeft: 10,
+    paddingRight: 10
+  }
 });
 export default styles;
