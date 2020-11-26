bb4fd6bb52b526e153906cc593e048a72c7a6342
Sachindeep Singh <Sachindeep.redsky@gmail.com>
2020-11-27 03:40:55 +0530

    fixed html wrap issue

--- src/screens/bulletindetails/index.js
+++ src/screens/bulletindetails/index.js
@@ -1,19 +1,33 @@
-import React, {Component} from 'react';
-import {View, Text, ScrollView} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import MainHeader from '../../components/MainHeader';
 import * as api from '../../api/bulletin-service';
 import styles from './styles';
 import HTMLView from 'react-native-htmlview';
+import { deviceWidth } from '../../utility'
+import { Dimensions, PixelRatio } from 'react-native';
+
+function renderNode(node, index, siblings, parent, defaultRenderer) {
+  console.log('RRRRRRRRR:R:R:R:R:R:',node)
+ 
+  // if (node.name == undefined) {
+  //   return null
+  // }
+}
+
 
 export default class BulletinDetail extends Component {
   constructor(props) {
     super(props);
     this.state = {
       bulletin: {},
-      header:'Finding Bulletin...',
+      header: 'Finding Bulletin...',
       isLoading: false,
       skip: 0,
+      width: PixelRatio.roundToNearestPixel(deviceWidth)
     };
+
+    console.log('WWWWWWW:W:WW:W:', this.state.width, this.width)
   }
   // state = {
   //   header: 'Abilify',
@@ -40,20 +54,22 @@ export default class BulletinDetail extends Component {
     this.getBullitenById(bullID);
   }
   getBullitenById = async (bullID) => {
-    this.setState({isLoading: true});
+    this.setState({ isLoading: true });
     let data = await api.bulletinByID(bullID);
     console.log('data..............................', data);
     this.setState({
       bulletin: data,
-      header:data.brandName,
+      header: data.brandName,
       isLoading: false,
     });
   };
+
+
   render() {
     return (
       <View style={styles.MainBox}>
         <MainHeader navigate={this.props.navigation} title={this.state.header} />
-        <ScrollView style={{margin: 12}}>
+        <ScrollView style={{ margin: 12 }}>
           <View style={styles.Item1}>
             <Text style={styles.datatext}>{'Generic Name:'}</Text>
             <Text style={styles.titleText}>
@@ -99,17 +115,23 @@ export default class BulletinDetail extends Component {
             </Text>
           </View>
           <View style={styles.Item}>
-            <View>
+            <View style={{ flexWrap: 'wrap' }}>
               <Text style={styles.datatext}>{'Approved Uses:'}</Text>
             </View>
             <Text style={styles.titleText1}>
-            {this.state.bulletin && this.state.bulletin.approvedUse ? (
-              <HTMLView
-              value={this.state.bulletin.approvedUse} style={styles.titleText1}>
-              </HTMLView>
-            ) : (
-              <Text style={styles.titleText1}>{'Finding Bulletin...'}</Text>
-            )}
+              {this.state.bulletin && this.state.bulletin.approvedUse ? (
+                <View style={{
+                  width: this.state.width - 25,
+                  flexWrap: 'wrap'
+                }}>
+                  <HTMLView
+                    addLineBreaks={false}
+                    value={this.state.bulletin.approvedUse} stylesheet={stylesHtml}>
+                  </HTMLView>
+                </View>
+              ) : (
+                  <Text style={styles.titleText1}>{'Finding Bulletin...'}</Text>
+                )}
             </Text>
           </View>
           <View style={styles.Item}>
@@ -117,13 +139,19 @@ export default class BulletinDetail extends Component {
               <Text style={styles.datatext}>{'Off-label Uses:'}</Text>
             </View>
             <Text style={styles.titleText1}>
-            {this.state.bulletin && this.state.bulletin.offLabelUse ? (
-              <HTMLView
-              value={this.state.bulletin.offLabelUse} style={styles.titleText1}>
-              </HTMLView>
-            ) : (
-              <Text style={styles.titleText1}>{'Finding Bulletin...'}</Text>
-            )}
+              {this.state.bulletin && this.state.bulletin.offLabelUse ? (
+                <View style={{
+                  width: this.state.width - 25,
+                  flexWrap: 'wrap'
+                }}>
+                  <HTMLView
+                    addLineBreaks={false}
+                    value={this.state.bulletin.offLabelUse} stylesheet={stylesHtml}>
+                  </HTMLView>
+                </View>
+              ) : (
+                  <Text style={styles.titleText1}>{'Finding Bulletin...'}</Text>
+                )}
             </Text>
           </View>
           <View style={styles.Item}>
@@ -131,28 +159,44 @@ export default class BulletinDetail extends Component {
               <Text style={styles.datatext}>{'Adverse Events:'}</Text>
             </View>
             {this.state.bulletin && this.state.bulletin.adverseEvents ? (
-              <HTMLView
-              value={this.state.bulletin.adverseEvents} style={styles.titleText1}>
+              <View style={{
+                width: this.state.width - 25,
+                flexWrap: 'wrap'
+              }}>
+             <HTMLView
+                addLineBreaks={false}
+                value={this.state.bulletin.adverseEvents} renderNode={renderNode}>
               </HTMLView>
+              </View>
             ) : (
-              <Text style={styles.titleText1}>{'Finding Bulletin...'}</Text>
-            )}
+                <Text style={styles.titleText1}>{'Finding Bulletin...'}</Text>
+              )}
           </View>
 
-          <View style={styles.Item}>
+          <View style={[styles.Item, { width: this.state.width - 25 }]}>
             <View>
               <Text style={styles.datatext}>{'Litigation:'}</Text>
             </View>
             {this.state.bulletin && this.state.bulletin.litigation ? (
               <HTMLView
-              value={this.state.bulletin.litigation} style={styles.titleText1}>
+                addLineBreaks={false}
+                value={this.state.bulletin.litigation} stylesheet={stylesHtml}>
               </HTMLView>
             ) : (
-              <Text style={styles.titleText1}>{'Finding Bulletin...'}</Text>
-            )}
+                <Text style={styles.titleText1}>{'Finding Bulletin...'}</Text>
+              )}
           </View>
         </ScrollView>
       </View>
     );
   }
 }
+
+const stylesHtml = StyleSheet.create({
+  body: {
+    fontWeight: '300',
+    color: '#FF3366', // make links coloured pink
+    margin: 0,
+    padding: 0
+  }
+});
